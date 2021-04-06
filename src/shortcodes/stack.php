ff88e1e6e74cc19a99f061485fa9608bb040fbf0
Andy Pearson <andy@londonparkour.com>
2021-04-06 14:07:46 +0100

    update

--- src/shortcodes/stack.php
+++ src/shortcodes/stack.php
@@ -4,9 +4,6 @@
  * Create the class and return results.
  */
 function andyp_pulse_stack_callback($atts){
-    
-    wp_register_style( 'andyp_stacks_css', ANDYP_PULSE_STACK_PATH.'src/sass/style.css' );
-    wp_enqueue_style( 'andyp_stacks_css' );
 
     $stack = new \andyp\pulsestack\REST\stack();
 
